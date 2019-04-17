97eccd73ba7fa99f5de8a579c13cbac24e975eb5
Clint.Network <me@clint.network>
2019-04-17 17:30:32 +0200

    Add section vue

---
+++ src/components/Section.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="py-5">
+    <div class="container">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Section'
+}
+</script>
+
+<style scoped>
+
+</style>
