0d10d0e6228aee2f2681f6664c056fd2efbf66b6
Clint.Network <me@clint.network>
2019-04-18 05:13:59 +0200

    Create dashboard vue

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <section style="background-color: #fafafa; padding: 500px;">
+            <div class="container">
+            </div>
+        </section>
+        <CallToAction content="Don't loose money, stake your coins on SeedStorm.io" button="Get Started" />
+    </div>
+</template>
+
+<script>
+// import Hero from '../components/Hero.vue'
+import CallToAction from '../components/CallToAction.vue'
+
+export default {
+  name: 'Dashboard',
+  components: {
+    //   Hero,
+      CallToAction
+  }
+}
+</script>
+
+<style scoped>
+section
+{
+    border-top: 1px solid #eeeeee;
+}
+</style>
