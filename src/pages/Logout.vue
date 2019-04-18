36517be886d544318ab15f667cf00901fb9b8db7
Clint.Network <me@clint.network>
2019-04-18 02:36:41 +0200

    Create Logout vue

---
+++ src/pages/Logout.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+import { endpoint } from '../environment.js'
+
+export default {
+  name: 'Logout',
+    //   CookieLaw
+  mounted()
+  {
+      localStorage.removeItem("Token")
+      this.$router.push("/dashboard")
+      location.reload()
+  }
+}
+</script>
+
+<style scoped>
+section
+{
+    border-top: 1px solid #eeeeee;
+    background-color: #f5f5f5;
+    padding: 130px 0 150px;
+    height: calc(100vh - 158px)
+}
+
+.inner
+{
+    width: 360px
+}
+
+.form-group
+{
+    text-align: left;
+}
+</style>
