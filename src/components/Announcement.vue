0d75497d1d8a167f374261182e48555bb79d001d
Clint.Network <me@clint.network>
2019-04-18 05:15:03 +0200

    Add news from API

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -5,7 +5,7 @@
                 <div id="loader" class="loader" style="height: 250px;" v-if="!this.loaded">
                     <half-circle-spinner :animation-duration="1000" :size="50" color="#555555" />
                 </div>
-                <transition name="fade" v-on:enter="enter">
+                <transition name="fade">
                     <hooper :autoPlay="true" :playSpeed="5000" v-if="this.loaded">
                         <slide v-for="announcement in announcements" :key="announcement.id">
                             <div class="row">
@@ -31,6 +31,7 @@
 </template>
 
 <script>
+import { endpoint } from '../environment.js'
 import axios from "axios"
 import { HalfCircleSpinner } from 'epic-spinners'
 import { Hooper, Slide } from 'hooper'
@@ -51,7 +52,7 @@ export default {
     },
     mounted() {
         axios
-            .get("https://localhost:5001/api/demo/announcements")
+            .get(endpoint + "/announces/announcements")
             .then(response => {
                 this.loaded = true;
                 this.announcements = response.data
