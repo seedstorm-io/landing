fac339c9d7c75cc77e95da87d5d145677deba31c
Clint.Network <me@clint.network>
2019-04-20 23:14:12 +0200

    Create markdown vue

---
+++ src/components/Markdown.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="content"></div>
+</template>
+
+<script>
+import marked from "marked"
+
+export default {
+    mounted()
+    {
+        document.getElementById("content").innerHTML = marked(this.$slots["default"][0]["text"])
+    }
+}
+</script>
