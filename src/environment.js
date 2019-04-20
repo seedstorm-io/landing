8ca7c19734077774980acfb00865faac6c2d2ec0
Clint.Network <me@clint.network>
2019-04-21 01:44:12 +0200

    Add method to check if user is logged in

--- src/environment.js
+++ src/environment.js
@@ -1,2 +1,10 @@
 export const endpoint = "http://localhost:7000/api"
 // export const endpoint = "http://api.seedstorm.io/api"
+
+export let isLogged = function()
+{
+    if (localStorage.getItem("Token")) {
+        return true;
+    }
+    return false;
+}
