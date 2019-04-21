efa9db38a939a194bb786c7229a56e4a2ae17595
Clint.Network <me@clint.network>
2019-04-21 02:10:21 +0200

    Add change culture feature

--- src/components/sub/LocaleChanger.vue
+++ src/components/sub/LocaleChanger.vue
@@ -1,27 +1,23 @@
 <template>
     <div class="dropdown-menu shadow border-0" aria-labelledby="lang-dropdown">
-        <a v-for="(lang, i) in langs" :key="`Lang${i}`" class="dropdown-item" href="#"><span :class="'flag-icon flag-icon-' + lang.flag"></span> &nbsp;{{ lang.name }}</a>
+        <a v-on:click="changeCulture(lang.culture)" v-for="(lang, i) in langs" :key="`Lang${i}`" class="dropdown-item" href="#"><span :class="`flag-icon flag-icon-${lang.flag}`"></span> &nbsp;{{ lang.name }}</a>
     </div>
 </template>
 
 <script>
 import '../../../node_modules/flag-icon-css/css/flag-icon.min.css'
+import { cultures } from '../../internationalization'
 
 export default {
     name: 'LocaleChanger',
+    methods: {
+        changeCulture(culture){
+            this.$i18n.locale = culture
+        }
+    },
     data() {
         return {
-            langs: [{
-                    'flag': 'fr',
-                    'culture': 'fr',
-                    'name': 'French'
-                },
-                {
-                    'flag': 'gb',
-                    'culture': 'en',
-                    'name': 'English'
-                }
-            ]
+            langs: cultures
         }
     }
 }
