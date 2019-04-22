bebb1cbd96d84552be44826039c20a5d58b384df
Clint.Network <me@clint.network>
2019-04-22 17:46:44 +0200

    Create ico page

---
+++ src/pages/ICO.vue
@@ -0,0 +1,14 @@
+<template>
+    <div style="padding: 500px 0;"></div>
+</template>
+
+<script>
+export default {
+  name: 'Error404',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
