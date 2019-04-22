d41ddaaa7619abf554a8963a2b1da77cf7e82d06
Clint.Network <me@clint.network>
2019-04-22 17:47:14 +0200

    Add active class for header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,26 +10,26 @@
             <div class="collapse navbar-collapse" id="navbarHeader">
                 <ul class="navbar-nav m-auto">
                     <li class="nav-item px-2">
-                        <router-link to="/" class="nav-link">{{ $t('header.home') }} <span class="sr-only">(current)</span></router-link>
+                        <router-link to="/" exact class="nav-link">{{ $t('header.home') }} <span class="sr-only">(current)</span></router-link>
                     </li>
                     <li class="nav-item px-2">
-                        <router-link to="/ico" class="nav-link">{{ $t('header.ico') }}</router-link>
+                        <router-link to="/ico" exact class="nav-link">{{ $t('header.ico') }}</router-link>
                     </li>
                     <li class="nav-item px-2">
-                        <router-link to="/services" class="nav-link">{{ $t('header.services') }}</router-link>
+                        <router-link to="/services" exact class="nav-link">{{ $t('header.services') }}</router-link>
                     </li>
                     <li class="nav-item px-2">
-                        <router-link to="/pricing" class="nav-link">{{ $t('header.pricing') }}</router-link>
+                        <router-link to="/pricing" exact class="nav-link">{{ $t('header.pricing') }}</router-link>
                     </li>
                     <li class="nav-item px-2">
                         <a href="//docs.seedstorm.io" target="_blank" class="nav-link">{{ $t('header.documentation') }}</a>
                     </li>
                     <li class="nav-item px-2">
-                        <router-link to="/support" class="nav-link">{{ $t('header.support') }}</router-link>
+                        <router-link to="/support" exact class="nav-link">{{ $t('header.support') }}</router-link>
                     </li>
                 </ul>
                 <form class="form-inline my-2 my-lg-0">
-                    <ButtonCTA to="create-account" v-if="!logged" value="$t('header.getstarted')" />
+                    <ButtonCTA to="create-account" v-if="!logged" :value="$t('header.getstarted')" />
                     <ButtonCTA to="dashboard" v-if="logged" :value="$t('header.mydashboard')" />
                 </form>
             </div>
@@ -84,6 +84,11 @@ export default {
     margin: 25px 0;
 }
 
+#header
+{
+    transition: box-shadow 0.3s ease-out;
+}
+
 .shrink
 {
     background-color: #FFFFFF;
@@ -91,8 +96,33 @@ export default {
     padding: 25px 0;
 }
 
-#header
-{
-    transition: box-shadow 0.3s ease-out;
+.navbar-light .navbar-nav .nav-link {
+  display: inline-block;
+  position: relative;
+  padding: 5px 0px;
+  margin: 8px;
+}
+
+/* .navbar-light .navbar-nav .nav-item.active .nav-link::after, */
+.navbar-light .navbar-nav .nav-link.router-link-active::after,
+.navbar-light .navbar-nav .nav-link::after
+ {
+  content: '';
+  position: absolute;
+  width: 100%;
+  transform: scaleX(0);
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(0,0,0,.5);
+  transform-origin: bottom right;
+  transition: transform 0.25s ease-out;
+}
+
+/* .navbar-light .navbar-nav .nav-item.active .nav-link::after, */
+.navbar-light .navbar-nav .nav-link.router-link-active::after,
+.navbar-light .navbar-nav .nav-link:hover::after {
+  transform: scaleX(1);
+  transform-origin: bottom left;
 }
 </style>
