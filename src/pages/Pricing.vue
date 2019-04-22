036a3169de899ece08fee8d9fab00eb25f997b25
Clint.Network <me@clint.network>
2019-04-22 17:46:59 +0200

    Create pricing vue

---
+++ src/pages/Pricing.vue
@@ -0,0 +1,130 @@
+<template>
+    <Section class="" mode="xs" style="padding-bottom: 100px !important;">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Our Pricing Plans</h2>
+                <p class="mb-4 pb-3">Find here all our brilliant nodes.</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="card border border-light shadow text-center price">
+                            <div class="card-body pt-0 pt-4 px-4 pb-3">
+                                <img class="mb-4 mt-3 icon" src="" />
+                                <h3 class="card-title">100€<small> /month</small>
+                                </h3>
+                                <h4>Business </h4>
+                                <p class="lead">For the best</p>
+                                <hr />
+                                <p>lol</p>
+                                <p>lol</p>
+                                <p>lol</p>
+                                <a href="#" class="btn my-4 btn-outline-primary">Purchase</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card border border-light shadow text-center price">
+                            <div class="card-body pt-0 pt-4 px-4 pb-3">
+                                <img class="mb-4 mt-3 icon" src="" />
+                                <h3 class="card-title">100€<small> /month</small>
+                                </h3>
+                                <h4>Business </h4>
+                                <p class="lead">For the best</p>
+                                <hr />
+                                <p>lol</p>
+                                <p>lol</p>
+                                <p>lol</p>
+                                <a href="#" class="btn my-4 btn-outline-primary">Purchase</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card border border-light shadow text-center price">
+                            <div class="card-body pt-0 pt-4 px-4 pb-3">
+                                <img class="mb-4 mt-3 icon" src="" />
+                                <h3 class="card-title">100€<small> /month</small>
+                                </h3>
+                                <h4>Business </h4>
+                                <p class="lead">For the best</p>
+                                <hr />
+                                <p>lol</p>
+                                <p>lol</p>
+                                <p>lol</p>
+                                <a href="#" class="btn my-4 btn-outline-primary">Purchase</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card shadow border border-light text-center price">
+                            <div class="card-body pt-0 pt-4 px-4 pb-3">
+                                <img class="mb-4 mt-3 icon" src="" />
+                                <h3 class="card-title">100€<small> /month</small>
+                                </h3>
+                                <h4>Business </h4>
+                                <p class="lead">For the best</p>
+                                <hr />
+                                <p>lol</p>
+                                <p>lol</p>
+                                <p>lol</p>
+                                <a href="#" class="btn my-4 btn-outline-primary">Purchase</a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <div class="col-lg-3">
+                        <div class="card shadow border-0 rounded-0 text-center price">
+                            <div class="card-body pt-0 pt-4 px-4 pb-3">
+                                <img class="mb-4 mt-3 icon" src="" />
+                                <h3 class="card-title">100€<small> /month</small>
+                                </h3>
+                                <h4>Business </h4>
+                                <p class="lead">For the best</p>
+                                <hr />
+                                <p>lol</p>
+                                <p>lol</p>
+                                <p>lol</p>
+                                <a href="#" class="btn my-4 btn-info"><i class="fas fa-shopping-cart"></i> Purchase</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card shadow border-0 rounded-0 text-center price">
+                            <div class="card-body pt-0 pt-4 px-4 pb-3">
+                                <img class="mb-4 mt-3 icon" src="" />
+                                <h3 class="card-title">100€<small> /month</small>
+                                </h3>
+                                <h4>Business </h4>
+                                <p class="lead">For the best</p>
+                                <hr />
+                                <p>lol</p>
+                                <p>lol</p>
+                                <p>lol</p>
+                                <a href="#" class="btn my-4 btn-info"><i class="fas fa-shopping-cart"></i> Purchase</a>
+                            </div>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+        </div>
+    </Section>
+</template>
+
+<script>
+import Section from '../components/Section.vue'
+import { endpoint } from '../environment.js'
+
+export default {
+  name: 'Pricing',
+  components: {
+      Section
+  },
+  mounted()
+  {
+  }
+}
+</script>
+
+<style scoped>
+</style>
