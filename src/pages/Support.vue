e11d434344f8088f2f8fcda31fb2492c83860f29
Clint.Network <me@clint.network>
2019-05-23 00:03:57 +0200

    Begin support page

--- src/pages/Support.vue
+++ src/pages/Support.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
-         <Section>
+    <div class="bg-white">
+        <Hero>
+            <div class="container">
+                <h1 class="display-4 native font-weight-500">
+                    {{ $t("support.title") }}
+                </h1>
+                <p v-html="$t('index.description', ['<a href=\'https://seedstorm.io\'>SeedStorm.io</a>'])"></p>
+                <div class="form-group">
+                    <Button to="/create-account" class="btn button btn-shadow btn-primary mr-2">
+                        <i class="fas fa-life-ring"></i>&nbsp;
+                        {{ $t('support.getsupport') }}
+                    </Button>
+                    <Button to="/explore" class="btn btn-shadow btn-dark ml-2">
+                        <i class="fas fa-book"></i>&nbsp;
+                        {{ $t('support.documentation') }}
+                    </Button>
+                </div>
+            </div>
+        </Hero>
+        <Section class="">
             <div class="mt-5 text-center">
                 <h2>{{ $t('index.faq') }}</h2>
                 <p class="mb-4 pb-3">{{ $t("index.faqdescription") }}</p>
@@ -11,13 +29,20 @@
 </template>
 
 <script>
+import { endpoint, isLogged } from '../environment.js'
 import Section from '../components/Section.vue'
+import Hero from '../components/Hero.vue'
 import FAQ from '../components/FAQ.vue'
+import CallToAction from '../components/CallToAction.vue'
+import Button from '../components/sub/Button.vue'
 
 export default {
     components: {
         Section,
-        FAQ
-    }
+        Hero,
+        FAQ,
+        CallToAction,
+        Button
+    },
 }
 </script>
